30f2a98e3e12a1b44731f6c2bc2282da383aeb4a
alvs-tora <64911289+alvs-tora@users.noreply.github.com>
2025-08-15 01:33:38 +0800

    Configuration File.

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__)) 
+UI_PATH = os.path.join(BASE_PATH, "gui", "ui")
+RESOURCES_PATH = os.path.join(BASE_PATH, "gui", "resources", "icon")
+MODEL_PATH = os.path.join(BASE_PATH, "detection", "model", "P3-best.pt")
+
+
+
+
+
+
